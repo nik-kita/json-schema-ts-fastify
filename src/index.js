68798a2a4aae3d4640f4e516226689bb50dc5d4a
nik <barrios.mazurka@gmail.com>
2021-09-22 18:27:19 +0300

    generate simple

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 const { program } = require('commander');
 const glob = require('glob');
 const path = require('path');
+const fs = require('fs');
+const { compile, compileFromFile } = require('json-schema-to-typescript');
 
 const defautMatchPattern = '*.schema.json';
 
+const compileSchemas = (schemas) => {
+  return new Promise((resolve, reject) => {
+    const _schemas = Array.isArray(schemas) ? schemas : [schemas];
+    const compilePromises = _schemas.map((file) => compileFromFile(file));
+
+    Promise.all(compilePromises)
+      .then((compiles) => {
+        const filePromises = compiles.map((compile, index) => new Promise((resolveWrite, rejectWrite) => {
+          fs.writeFile(_schemas[index].replace(/json$/, 'ts'), compile, { encoding: 'utf8' }, (err, data) => {
+            if (err) rejectWrite(err);
+            else resolveWrite(data);
+          });
+        }));
+
+        Promise.all(filePromises).then((data) => resolve(data));
+      });
+  });
+}
+
 const matchFiles = (pattern) => {
-  glob(path.join(__dirname, '..',`!(node_modules)/**/${pattern}`), (err, files) => {
-    console.log(files);
+  return new Promise((resolve, reject) => {
+    glob(path.join(__dirname, '..', `!(node_modules)/**/${pattern}`), (err, files) => {
+      if (err) reject(err);
+      else resolve(files);
+    });
   });
 }
 
 program
   .command('generate')
   .option('-p <pattern>', 'pattern for match json-schema files', defautMatchPattern)
-  .action((args) => {
+  .action(async (args) => {
     console.log('PATTERN', args);
 
-    matchFiles(args.p);
+    const schemas = await matchFiles(args.p);
+    console.log(schemas);
+    await compileSchemas(schemas);
   });
 
 
