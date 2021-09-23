5b8f7eef47f68f997aa48e8f34b5397ba920bae0
nik <barrios.mazurka@gmail.com>
2021-09-23 16:25:43 +0300

    eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { program } from "commander";
-import { convert } from "./main";
-import { UserOptions } from "./types";
+import { program } from 'commander';
+import { convert } from './main';
 
 program
   .option(
@@ -9,11 +8,11 @@ program
   )
   .option(
     '-p, --prefix <prefix>',
-    'prefix to use before interface\'s name'
+    'prefix to use before interface\'s name',
   )
   .option(
     '-e, --ext <extension>',
-    'file extension to use for generated files'
+    'file extension to use for generated files',
   )
   .option(
     '-m, --module <module>',
@@ -21,22 +20,20 @@ program
   )
   .option(
     '-b, --banner <banner>',
-    'upper comment in generated files'
+    'upper comment in generated files',
   )
   .action((options) => {
     const _options = {
       ...{
-        glob: "!(node_modules|dist)/**/*schema.json",
-        prefix: "",
-        ext: ".ts",
-        module: "fastify",
+        glob: '!(node_modules|dist)/**/*schema.json',
+        prefix: '',
+        ext: '.ts',
+        module: 'fastify',
         bannerComment: '/** Hello */',
       },
       ...options,
     };
 
     convert(_options);
-  })
-  ;
-
+  });
 program.parse(process.argv);
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
-import { matchFiles } from "./file.utils";
-import { UserOptions } from "./types";
 import fs from 'fs';
-import { writeFile } from "./helpers";
-import { generateInterfaces } from "./generate-interfaces.function";
-
+import { matchFiles } from './file.utils';
+import { generateInterfaces } from './generate-interfaces.function';
+import { writeFile } from './helpers';
+import { UserOptions } from './types';
 
 export async function convert(options: UserOptions) {
-
   const parsedFiles = await matchFiles(options.glob);
 
   await Promise.all([
     parsedFiles.map(async (parsedFile) => {
-      const schema = JSON.parse(fs.readFileSync(parsedFile.dir + '/' + parsedFile.base, 'utf8'));
+      const schema = JSON.parse(fs.readFileSync(`${parsedFile.dir}/${parsedFile.base}`, 'utf8'));
       const matchName = parsedFile.name.match(/^[^\.]+/);
       const name = matchName ? matchName[0] : '';
       const template = await generateInterfaces(schema, options, name);
       await writeFile(parsedFile, template, options);
-    })
+    }),
   ]);
-
 }
