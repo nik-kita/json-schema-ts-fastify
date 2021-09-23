d8d159856e87301cfc9f65d35cf6415dcfdbe3b9
nik <volsknik@gmail.com>
2021-09-23 07:59:18 +0300

    compypaster

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import { glob } from 'glob';
 import { compileFromFile } from 'json-schema-to-typescript';
 import path from 'path';
+import { convert, defaultOptions } from './schema';
 
 
 const defautMatchPattern = '*.schema.json';
@@ -40,14 +41,64 @@ const matchFiles = (pattern: string) => {
 
 program
   .command('generate')
-  .option('-p <pattern>', 'pattern for match json-schema files', defautMatchPattern)
+  .option(
+    "-g, --glob <value>",
+    "glob matching JSON schema to convert",
+    defaultOptions.glob
+  )
+  // .option(
+  //   "-p, --prefix <value>",
+  //   "prefix to use before interfaces' name",
+  //   parsePrefix,
+  //   defaultOptions.prefix
+  // )
+  // .option(
+  //   "-e, --ext <value>",
+  //   "file extension to use for generated files",
+  //   parseExtension,
+  //   defaultOptions.ext
+  // )
+  // .option(
+  //   "-m, --module <value>",
+  //   "module to import the RouteHandler type from",
+  //   defaultOptions.module
+  // )
   .action(async (args) => {
     console.log('PATTERN', args);
 
     const schemas = await matchFiles(args.p);
     console.log(schemas);
     await compileSchemas(schemas);
-  });
+  })
+  ;
 
 
-program.parse(process.argv);
+  // program.parse(process.argv);
+  // const options = program.opts();
+  
+  // convert({
+  //   glob: options.glob,
+  //   prefix: options.prefix,
+  //   ext: options.ext,
+  //   module: options.module,
+  // });
+  
+
+
+function parsePrefix(value: string) {
+  if (!value.match(/^\w/i)) {
+    console.error("Prefix needs to start with a letter");
+    process.exit(-1);
+  }
+
+  return value;
+}
+
+function parseExtension(value: string) {
+  if (!value.match(/^\./)) {
+    console.error('File extension needs to start with a "."');
+    process.exit(-1);
+  }
+
+  return value;
+}
